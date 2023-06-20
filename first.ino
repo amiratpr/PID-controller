b6e5b8cfe38ed9ff893b352ce11068c0f7982be5
Amir Atapour <amiratpr@gmail.com>
2023-06-20 16:08:06 +0330

    Update first.ino
    PID solved

--- first.ino
+++ first.ino
@@ -1,93 +1,80 @@
-
-const int pumpPin = 9;              // PWM pin for controlling pump speed
-const int pin1 = 6;                 //direction pins
-const int pin2 = 7;                 //direction pins
-const int triggerPin = 2;           // Ultrasonic sensor trigger pin
-const int echoPin = 3;              // Ultrasonic sensor echo pin
-
-// PID Constants
-const float Kp = 0.5;               // Proportional gain
-const float Ki = 0.2;               // Integral gain
-const float Kd = 0.1;               // Derivative gain
-
-// Variables
-const int targetLevel = 8;        // Desired water level (adjust as needed)
-int pumpSpeed = 0;                  // Initial pump speed (0-255)
-float waterLevel = 0;               // Current water level
-int previousError = 0;              // Previous error for derivative term
-float integralTerm = 0;             // Integral term for integral term
-unsigned long previousTime = 0;     // Previous time for derivative term
 #include "SRF05.h"
-SRF05 SRF(triggerPin,echoPin);
+
+// Pin connections
+const int triggerPin = 2;    // Trigger pin of SRF05
+const int echoPin = 3;       // Echo pin of SRF05
+const int pumpPin = 9;       // Control pin for water pump
+
+
+// PID constants
+const double Kp = 2.0;       // Proportional gain
+const double Ki = 0.5;       // Integral gain
+const double Kd = 0.1;       // Derivative gain
+
+// Setpoint and variables
+const double setpoint = 7;  // Desired water level in centimeters
+double currentLevel = 0;     // Current water level in centimeters
+double error = 0;            // Error term
+double previousError = 0;    // Previous error term
+double integral = 0;         // Integral term
+double derivative = 0;       // Derivative term
+double output = 0;           // Output signal for the pump
+
+// Timing variables
+unsigned long previousTime = 0;
+const int sampleTime = 100;  // Sample time in milliseconds
+
+// Create an instance of the SRF05 class
+SRF05 SRF05(triggerPin, echoPin);
+
 void setup() {
-  // Initialize pump and ultrasonic sensor pins
+  // Initialize the pump pin as an output
   pinMode(pumpPin, OUTPUT);
-  pinMode(pin1, OUTPUT);
-  pinMode(pin2, OUTPUT);
-  pinMode(triggerPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  // Start serial communication for debugging (optional)
   Serial.begin(9600);
-  SRF.setCorrectionFactor(1.035);
-  SRF.setModeAverage(10);
+
+  // Initialize the SRF05 sensor
+  
 }
 
 void loop() {
-   digitalWrite(pin1,HIGH);
-   digitalWrite(pin2,LOW);
-  // Read water level using ultrasonic sensor
-  waterLevel = 14-SRF.getCentimeter();
 
-  // Calculate the error
-  int error = targetLevel - waterLevel;
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  // Read the current water level from the SRF05 sensor
+  currentLevel = 14 - SRF05.getCentimeter();
+  
+
+  // Calculate the error term
+  error = setpoint - currentLevel;
 
   // Calculate the time difference
   unsigned long currentTime = millis();
-  float timeDiff = (currentTime - previousTime) / 1000.0; // Convert to seconds
+  double timeDiff = (currentTime - previousTime) / 1000.0; // Convert to seconds
 
-  // Proportional term
-  float proportionalTerm = Kp * error;
+  // PID calculations
+  integral += error * timeDiff;
+  derivative = (error - previousError) / timeDiff;
+  output = Kp * error + Ki * integral + Kd * derivative;
 
-  // Integral term
-  integralTerm += Ki * error * timeDiff;
+  // Limit the output signal to the allowed range
+  output = constrain(output, 0, 255);
 
-  // Derivative term
-  float derivativeTerm = Kd * (error - previousError) / timeDiff;
-
-  // Calculate the pump speed using PID control
-  pumpSpeed = proportionalTerm + integralTerm + derivativeTerm;
-
-  // Limit the pump speed to prevent overflow
-  pumpSpeed = constrain(pumpSpeed, 0, 255);
-
-  // Control the pump speed
-  analogWrite(pumpPin, pumpSpeed);
-
-  // Print the water level, pump speed, and PID terms for debugging (optional)
-  Serial.print("Water Level: ");
-  Serial.print(waterLevel);
-  Serial.print("  Pump Speed: ");
-  Serial.print(pumpSpeed);
-  Serial.print("  P: ");
-  Serial.print(proportionalTerm);
-  Serial.print("  I: ");
-  Serial.print(integralTerm);
-  Serial.print("  D: ");
-  Serial.println(derivativeTerm);
-  Serial.print("  ERROR: ");
-  Serial.println(error);
-
-  // Update previous error and time for the next iteration
+  // Update the previous error and time variables
   previousError = error;
   previousTime = currentTime;
 
-  // Check if the target level is reached
-  if (waterLevel >= targetLevel) {
-    // Stop the pump
-    analogWrite(pumpPin, 0);
-    integralTerm=0;
-  }
+  // Control the water pump based on the output signal
+  analogWrite(pumpPin, output);
 
-  // Delay between measurements
-  delay(1000);
+  Serial.print("Water Level: ");
+  Serial.print(currentLevel);
+  Serial.print("  Pump Speed: ");
+  Serial.print(output);
+  Serial.print("  I: ");
+  Serial.print(integral);
+  Serial.print("  D: ");
+  Serial.println(derivative);
+
+  // Delay for the sample time
+  delay(sampleTime);
 }
